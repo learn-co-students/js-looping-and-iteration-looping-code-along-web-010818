f352c1dde6b3a57c9398247b25869d1336a833dc
Fei <fei.hafferkamp@gmail.com>
2018-02-19 10:51:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log('Welcome ' + names[i] + '! You are employee #' + (i+1) + '.')
+  }
+  return names;
+}
+
+function tailsNeverFails() {
+  let count = 0;
+  while (Math.round(Math.random()) !== 1) {
+    count += 1;
+  }
+  console.log('you got ' + count + ' tails in a row!');
+}
